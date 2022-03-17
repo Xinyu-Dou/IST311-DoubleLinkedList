3567ade1e5bf8885d3614b476663c46a2da54d47
BuildTools <unconfigured@null.spigotmc.org>
2022-03-16 21:49:00 -0400

    Create insert method

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -26,4 +26,49 @@ public class DoubleLinkedList<T> {
         return node;
     }
 
+    //insert at that point in the list (0 is the head, 1 is right after the head)
+    public Node<T> insert(int position, Album data) {
+        Node<T> node = new Node<T>(data);
+
+        try {
+            if (position > numNode || position < 0) {
+                throw new IllegalArgumentException();
+            }
+            // add to the end of the list
+            else if (position == numNode) {
+                append(data);
+            } else {
+                // nothing in the list, start the list
+                if (position == 0 && numNode == 0) {
+                    this.head = node;
+                    this.tail = node;
+                }
+                // add to the head of the list
+                else if (position == 0) {
+                    this.head.next = this.head;
+                    this.head.next.prev = this.head;
+                    this.head = node;
+                }
+                // add to the middle of the list
+                else if (position < numNode) {
+                    int count = 1;
+                    Node<T> pre_node = this.head;
+                    Node<T> after_node = this.head.next;
+                    while (count < position) {
+                        pre_node = after_node;
+                        after_node = after_node.next;
+                    }
+                    pre_node.next = node;
+                    pre_node.next.prev = pre_node;
+                    pre_node.next.next = after_node;
+                    pre_node.next.next.prev = node;
+                }
+                numNode++;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Location is out of the bounds of the list");
+        }
+        return node;
+    }
+
 }
--- src/DoubleLinkedListTest.java
+++ src/DoubleLinkedListTest.java
@@ -8,7 +8,24 @@ class DoubleLinkedListTest {
 
     @org.junit.jupiter.api.Test
     void append() {
+        DoubleLinkedList dl = new DoubleLinkedList();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("First Name");
+        list.add("Second Name");
+        Album a1 = new Album(1,list,"Chen",1);
+        dl.append(a1);
+    }
 
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+        DoubleLinkedList dl = new DoubleLinkedList();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("First Name");
+        list.add("Second Name");
+        Album a1 = new Album(1,list,"Chen",1);
+        dl.insert(0,a1);
+        System.out.println(dl.toString());
     }
 
 }
